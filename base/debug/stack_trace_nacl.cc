7349cb11f7e42101b258fd6ccafa2fff823b9499
Eric Seidel <github@eseidel.com>
2010-11-12 11:01:35 -0800

    add stack_trace_nacl

---
+++ base/debug/stack_trace_nacl.cc
@@ -0,0 +1,20 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/debug/stack_trace.h"
+
+namespace base {
+namespace debug {
+
+StackTrace::StackTrace() {
+}
+
+void StackTrace::PrintBacktrace() {
+}
+
+void StackTrace::OutputToStream(std::ostream*) {
+}
+
+}  // namespace debug
+}  // namespace base
