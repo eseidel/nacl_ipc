b4719df708144e20f3e0f3a2aa2db67dda9af624
Eric Seidel <github@eseidel.com>
2010-11-15 00:22:19 -0800

    make us even more sad that we don't support stacktraces

--- base/debug/stack_trace_nacl.cc
+++ base/debug/stack_trace_nacl.cc
@@ -2,18 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/logging.h"
 #include "base/debug/stack_trace.h"
 
 namespace base {
 namespace debug {
 
 StackTrace::StackTrace() {
+  NOTIMPLEMENTED();
 }
 
 void StackTrace::PrintBacktrace() {
+  NOTIMPLEMENTED();
 }
 
 void StackTrace::OutputToStream(std::ostream*) {
+  NOTIMPLEMENTED();
 }
 
 }  // namespace debug
