585f72737f292acaca7aabcc1e0b5a1ee297e77a
Adam Barth <abarth@quadzen.local>
2010-11-30 23:31:59 -0800

    Dependency tracking

--- library.mk
+++ library.mk
@@ -3,5 +3,13 @@ all: $(PROJECT_ROOT)/lib$(LIBRARY_NAME).a
 $(PROJECT_ROOT)/lib$(LIBRARY_NAME).a: $(OBJECTS) $(TEST_OBJECTS)
 	$(AR) cr $@ $^ 
 
+depend: $(OBJECTS:%.o=%.cc) $(TEST_OBJECTS:%.o=%.cc)
+	rm -f ./.depend
+	$(CC) $(CFLAGS) $(INCLUDES) -MM $^>>./.depend;
+
+.depend: depend
+
+include .depend
+
 clean:
 	$(RM) $(OBJECTS) $(TEST_OBJECTS) $(PROJECT_ROOT)/lib$(LIBRARY_NAME).a
